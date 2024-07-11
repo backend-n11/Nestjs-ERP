9683f9cbc9302632623250382e0dfa59b20c056b
reydor008 <uzbbnbf@gmail.com>
2024-07-11 09:02:11 +0500

    files path added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { CoursesModule } from './courses/courses.module';
+import { FilesModule } from './files/files.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, AuthModule, CoursesModule],
+  imports: [ConfigModule.forRoot({envFilePath : ".env", isGlobal : true}), UsersModule, AuthModule, CoursesModule, FilesModule],
   controllers: [],
   providers: [],
 })
